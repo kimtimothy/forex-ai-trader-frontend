a6f52b0d4952ddce0ecfef18231b0610c71b0d26
Tim <50609261+kimtimothy@users.noreply.github.com>
2025-03-09 17:31:31 -0400

    update endpoint

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,1 +1,1 @@
-export const API_URL = process.env.REACT_APP_API_URL || 'https://forex-ai-trade-backend.vercel.app/'; 
+export const API_URL = process.env.REACT_APP_API_URL || 'https://forex-ai-trader-backend-0b07293d3688.herokuapp.com'; 
